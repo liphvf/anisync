38ce20e98cc2ead3fc7ccb04b8d1a40033762541
Filiphe Vilar Figueiredo <liphvf@outlook.com>
2022-03-04 00:48:37 -0300

    estudo incial

---
+++ Models/Kitsu/GetUserByNicknameResponse.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace anisync.Models.Kitsu
+{
+    public class GetUserByNicknameResponse
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System.Net.Http.Json;
+using anisync.Models.Kitsu;
+
+Console.WriteLine("Informe nome do usuário:");
+var nomeUsuario = Console.ReadLine();
+
+var urlPegarNomeUsuario = $"https://kitsu.io/api/edge/users?filter[name]={nomeUsuario}&fields[users]=id";
+
+var httpClient = new HttpClient();
+
+var usuario = await httpClient.GetFromJsonAsync<ResponseKitsu<GetUserByNicknameResponse>>(urlPegarNomeUsuario);
+
+Console.WriteLine("");
+
+
+var libraryEntries = await httpClient.GetFromJsonAsync($"https://kitsu.io/api/edge/users/{usuario.Data.First().Id}/library-entries/?fields%5Banime%5D=slug%2CcanonicalTitle%2Ctitles&fields%5BlibraryEntries%5D=createdAt,updatedAt,status,progress,volumesOwned,reconsuming,reconsumeCount,notes,private,reactionSkipped,progressedAt,startedAt,finishedAt,rating,ratingTwenty,anime,manga&fields%5Bmanga%5D=slug%2CcanonicalTitle%2Ctitles&filter%5Bkind%5D=anime,manga&page%5Boffset%5D=0&page%5Blimit%5D=100&include=anime,manga");
+// 1
+//
+//
