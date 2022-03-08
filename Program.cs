9b4f46647f61f049395d6ee5e2165a20afa61a60
Filiphe Vilar Figueiredo <liphvf@outlook.com>
2022-03-07 21:33:42 -0300

    another prove of concept

--- Program.cs
+++ Program.cs
@@ -1,9 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-
-using System.Linq;
+﻿using System.Linq;
 using System.Net.Http.Json;
 using anisync.Models.Kitsu;
+using GraphQL.Client;
+using Microsoft.Extensions.DependencyInjection;
+using StrawberryShake;
+
+IServiceCollection serviceCollection = new ServiceCollection();
+
+serviceCollection.AddGraphQlClient(StrawberryShake.ExecutionStrategy.NetworkOnly).ConfigureHttpClient(e => e.BaseAddress = new Uri("https://graphql.anilist.co/"));
+
+IServiceProvider serviceProvider = serviceCollection.BuildServiceProvider();
+
+IGraphQlClient client = serviceProvider.GetRequiredService<IGraphQlClient>();
+
+IOperationResult<IGetUserIdResult> result = await client.GetUserId.ExecuteAsync();
+
+if (result.IsErrorResult())
+{
+    Console.WriteLine("falou");
+}
+else
+{
+    Console.WriteLine(result.Data?.User?.Id);
+}
 
 Console.WriteLine("Informe nome do usuário:");
 // var nomeUsuario = Console.ReadLine();
