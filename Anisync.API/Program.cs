bbd1e7797d0d9087f43c5575995d3b5dd0b44ecc
Filiphe Vilar Figueiredo <liphvf@outlook.com>
2022-03-15 00:08:45 -0300

    commit for branch change

--- Anisync.API/Program.cs
+++ Anisync.API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options => {
+   options.TokenValidationParameters = new TokenValidationParameters {
+       
+   };
+});
 
 var app = builder.Build();
 
