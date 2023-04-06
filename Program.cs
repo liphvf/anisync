2b2695e1e1c07a9908ec5cd8c2a736c36159b03c
Filiphe Vilar Figueiredo <liphvf@outlook.com>
2023-04-06 00:22:26 -0300

    code ideia

--- Program.cs
+++ Program.cs
@@ -1,10 +1,11 @@
 ﻿using System.Linq;
 using System.Net.Http.Json;
 using anisync.Models.Kitsu;
-using GraphQL.Client;
+// using GraphQL.Client;
 using Microsoft.Extensions.DependencyInjection;
 using StrawberryShake;
 using System.Net;
+using AnilistClient.Graphql;
 
 Console.WriteLine("Olá esse aplicativo é utilizado para sincronizar seus animes do kitsu para anilist");
 Console.WriteLine("Para começar primeiro precisamos de autorização para acessar sua biblioteca. Para isso acesse:");
@@ -13,21 +14,32 @@ Console.WriteLine("Após autenticar, copie o código e cole abaixo:");
 
 // var anilistCode = Console.ReadLine();
 
-var anilistCode = "def502002156abde6b81b087501ecfcd1e3af007ecb372e528dda30f3a004592d8254c8d9b75d61fe51e4dccf696b17c3b75622ea76d510eb66bee42072b0a487c645bfaec2271d18b3b5223cf8889e89e347fcde3b003f1d288d3b9b700fa26af0fd9c22748d6e3f556b04dd0d69270c619991437606af254e4056c23a2d54fe3e61c2858b671a69d5ec750f3a6a7ac0ca32e97847fca847b8cb6bf05a82eb88b72a29fb8d00c75d4e969b521fd134fde27033104ff12e0baedfe3e88e894236d715c24fc0bc1d14238c7913605aa8577821b5f76b833f8db9a21f8afd30c0553fe3c51812b9de3e1a42ed6eaaafc3b3ec5ece420c2bfbf0fb0c9b0600736879321f92ce3abccf82bc34eae3f29290852516ecc01d883a17570808d59a14171c2d4cb38cc5e61638cba64117c12bf3985ff575f4e711d597e8bd46252a434ede19bda82cdbf26c288769c79a08ee026aa2109fb6b2614f884b55a0b145b6554d82847528717aa078ab25998bea7608e4d";
-
 
 IServiceCollection serviceCollection = new ServiceCollection();
+            serviceCollection
+                .AddAnilistClient()
+                .ConfigureHttpClient(client => client.BaseAddress = new Uri("https://workshop.chillicream.com/graphql"));
 
-serviceCollection.AddGraphQlClient(StrawberryShake.ExecutionStrategy.NetworkOnly).ConfigureHttpClient(e => e.BaseAddress = new Uri("https://graphql.anilist.co/"));
+            IServiceProvider services = serviceCollection.BuildServiceProvider();
 
-IServiceProvider serviceProvider = serviceCollection.BuildServiceProvider();
+            IAnilistClient client = services.GetRequiredService<IAnilistClient>();
 
-IGraphQlClient client = serviceProvider.GetRequiredService<IGraphQlClient>();
+// IServiceCollection serviceCollection = new ServiceCollection();
+//     serviceCollection
+//         .AddAnilistClient()
+//         .ConfigureHttpClient(client => client.BaseAddress = new Uri("https://workshop.chillicream.com/graphql"));
+
+// serviceCollection.AddAnilistClient(StrawberryShake.ExecutionStrategy.NetworkOnly).ConfigureHttpClient(e => e.BaseAddress = new Uri("https://graphql.anilist.co/"));
+
+// IServiceProvider serviceProvider = serviceCollection.BuildServiceProvider();
+
+// IGraphQlClient client = serviceProvider.GetRequiredService<IGraphQlClient>();
 
 IOperationResult<IGetUserIdResult> result = await client.GetUserId.ExecuteAsync("liphvf");
 
 if (result.IsErrorResult())
 {
+    var xuxu = result.Errors;
     Console.WriteLine("falou");
 }
 else
